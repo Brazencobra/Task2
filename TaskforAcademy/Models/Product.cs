68c0362c12f72d60b3c80ac2d06f7ed582b9d0f3
John Doe <bossfatya@gmail.com>
2022-11-24 08:42:57 +0400

    First commit

---
+++ TaskforAcademy/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskforAcademy.Models
+{
+    internal class Product
+    {
+        public string Name;
+        private decimal _price;
+
+        public decimal Price 
+        { 
+            get { return _price; } 
+            set 
+            {
+                if (value >= 0)
+                {
+                    _price = value;
+                } 
+            } 
+        } 
+
+        public Product(string name,decimal price)
+        {
+            Name = name;
+            _price = price;
+        }
+    }
+}
---
+++ TaskforAcademy/Models/User.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskforAcademy.Models
+{
+    internal class User
+    {
+        public string UserName;
+        string _password;
+        private bool _isSignedIn;
+        public string Products;
+
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value.Length >= 8 && value.Length <= 20)
+                {
+                    _password = value;
+                }
+            }
+        }
+        public bool IsSignedIn 
+        {
+            get
+            {
+                return _isSignedIn;
+            }
+            set
+            {
+                if (CheckPassword(value) == true)
+                {
+                    _isSignedIn = true;
+                }
+            }
+        }
+        
+
+        public (string,bool) CheckPassword(string text)
+        {
+            if (HasDigit(text) == true && HasLower(text) == true && HasUpper(text) ==true)
+            {
+                return ("Sifrenizi dogru daxil etmisiniz",true);
+            }
+            else
+            {
+                return ("Sifrenizi sehv daxil etmisiniz",false);
+            }
+        }
+        public bool HasDigit(string text)
+        {
+            bool result = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    result = true;
+                }
+            }
+            return result;
+
+        }
+        public bool HasLower(string text)
+        {
+            bool result = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLower(text[i]))
+                {
+                    result = true;
+                }
+            }
+            return result;
+
+        }
+        public bool HasUpper(string text)
+        {
+            bool result = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]))
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+
+        public User(string username, string password,string products,string password)
+        {
+            UserName = username;
+            _password = password;
+            Products = products;
+        }
+
+        
+    }
+
+}
